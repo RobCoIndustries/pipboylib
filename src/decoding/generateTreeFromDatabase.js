bfce721d07cd0537a74427487d7d46ffb331b812
Phil Plückthun <phil@plckthn.me>
2015-11-27 14:33:44 +0100

    Remove unrelated immutable.js code from generateTreeFromDatabase

--- src/decoding/generateTreeFromDatabase.js
+++ src/decoding/generateTreeFromDatabase.js
@@ -1,13 +1,13 @@
 export default function generateTreeFromDatabase(db, key = 0) {
   const rec = sub => {
     if (Array.isArray(sub)) {
-      return new List(sub.map(x => rec(db[x])))
+      return sub.map(x => rec(db[x]))
     } else if(typeof sub === 'object') {
       const res = {}
       for (let key in sub) {
         res[key] = rec(db[sub[key]])
       }
-      return new Map(res)
+      return res
     }
 
     return sub
